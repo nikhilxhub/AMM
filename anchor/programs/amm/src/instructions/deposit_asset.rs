e78e87bb557975ff522abe544fe4f8f3ea2a6903
nikhilxhub <ummidinikhil@gmail.com>
2026-02-25 23:20:44 +0530

    hehe

--- anchor/programs/amm/src/instructions/deposit_asset.rs
+++ anchor/programs/amm/src/instructions/deposit_asset.rs
@@ -1,4 +1,12 @@
 use anchor_lang::prelude::*;
+use crate::{error::AMMError, state::Config};
+
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{mint_to, transfer, Mint, MintTo, Token, TokenAccount, Transfer}
+};
+
+use constant_product_curve::ConstantProduct;
 
 #[derive(Accounts)]
 pub struct Deposit<'info> {
@@ -7,6 +15,63 @@ pub struct Deposit<'info> {
 
     pub mint_x:Account<'info,Mint>,
     pub mint_y:Account<'info,Mint>,
+
+    #[account(
+        seeds = [b"config", config.seed.to_le_buytes().as_ref()],
+        bump = config.config_bump,
+        has_one = mint_x,
+        has_one = mint_y,
+    )]
+    pub config: Account<'info, Config>,
+
+    #[account(
+        mut,
+        seeds = [b"lp", conig.key().as_ref()],
+        bump = conig.lp_bump,
+    )]
+    pub mint_lp:Account<'info, Mint>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = config,
+    )]
+    pub vault_x: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = config,
+    )]
+    pub vault_y: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_x,
+        associated_token::authority = user,
+    )]
+    pub user_x: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_y,
+        associated_token::authority = user,
+    )]
+    pub user_y: Account<'info, TokenAccount>,
+
+    #[account(
+        mut,
+        associated_token::mint = mint_lp,
+        associated_token::authority = user,
+    )]
+    pub user_lp: Account<'info, TokenAccount>,
+
+    pub token_program: Program<'info, Token>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub system_program: Program<'info, System>,
+
+
+
 
 }
 
